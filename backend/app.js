283bdea7139a60b1a082aa59784ef52b5bc2df65
beauverger <beauverger.rodolphe@orange.fr>
2021-06-05 08:47:58 +0200

    problème CORS

--- backend/app.js
+++ backend/app.js
@@ -10,14 +10,7 @@ const app = express();
 
 app.use(helmet());
 
-/* app.use(cors()); */
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  next();
-});
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
